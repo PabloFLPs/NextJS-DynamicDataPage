a9c3fc21e3f34714a24119a95b972ca87ed6ad14
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-02-14 18:52:21 -0300

    Adding Home component and styling file

---
+++ components/Home.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Home.module.css"
+
+export const Home = (props) => {
+
+    return (
+        <div className={styles.main}>
+            <h1>WHY CHOOSE US</h1>
+            <span>Moles appear in all shapes and sizes? We can help!</span>
+        </div>
+    )
+}
