5c9869662779f58201312a8873c37a5cd2ba27ce
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-02-14 18:04:44 -0300

    Changing how we get props

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@
 export const Header = (props) => {
     return (
         <div>
-            <h1>Get {props.value?.service.name || "[service]"} to {props.value?.city.name || "[city]"} </h1>
+            <h1>Get {props.value.service || "[service]"} to {props.value.city || "[city]"} </h1>
         </div>
     )
 }
