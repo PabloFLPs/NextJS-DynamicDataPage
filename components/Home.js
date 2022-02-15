da95802f7b7210ec98d0ea36d9278c67a78e308a
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-02-15 18:08:16 -0300

    Finishing initial Home component page structure

--- components/Home.js
+++ components/Home.js
@@ -1,11 +1,65 @@
 import styles from "../styles/Home.module.css"
 
+import { HiOutlineSearch, HiOutlineSun } from "react-icons/hi";
+import { AiFillCar } from "react-icons/ai";
+
+import Image from 'next/image'
+import BefAftImage from "../public/assets/derm-before-after.jpg"
+
 export const Home = (props) => {
 
     return (
         <div className={styles.main}>
-            <h1>WHY CHOOSE US</h1>
-            <span>Moles appear in all shapes and sizes? We can help!</span>
+            <div className={styles.slogan}>
+                <h2 className={styles.titleMessage}>
+                    <strong>WHY CHOOSE US</strong>
+                </h2>
+                <p className={styles.message}>
+                    <span>{props.value?.service.replace("Removal", "") || "[service]"} appear in all shapes and sizes? We can help!</span>
+                </p>
+            </div>
+            <div className={styles.iconsSection}>
+                <div className={styles.itemsContainer}>
+                    <HiOutlineSearch className={styles.icons}/>
+                    <h3 className={styles.iconSectionTitle}>
+                        <span>Full Body Skin Exams</span>
+                    </h3>
+                    <p className={styles.iconDescription}>
+                        <span>Using cutting-edge dermoscopy techniques, let us be your safety net after all those years in the dangerous sun. Skin cancer is not your friend, but we are.</span>
+                    </p>
+                </div>
+                <div className={styles.itemsContainer}>
+                    <HiOutlineSun className={styles.icons}/>
+                    <h3 className={styles.iconSectionTitle}>
+                        <span>Free of Pain, Full of Comfort</span>
+                    </h3>
+                    <p className={styles.iconDescription}>
+                        <span>Go back to wearing your favourite clothes, no more obstacles in your way! Guaranteed results, satisfied customers.</span>
+                    </p>
+                </div>
+                <div className={styles.itemsContainer}>
+                    <AiFillCar className={styles.icons}/>
+                    <h3 className={styles.iconSectionTitle}>
+                        <span>Doctor to You!</span>
+                    </h3>
+                    <p className={styles.iconDescription}>
+                        <span>Mobile specialists offering in-home services as an option to you. Professional care in the comfort of your home.</span>
+                    </p>
+                </div>
+            </div>
+            <div className={styles.contactPhone}>
+                <span>Do you need a FREE consultation? <strong>Call us now!</strong></span>
+                <div className={styles.buttonContainer}>
+                    <button type="button">Call Us: +1 (289) 206-0046</button>
+                </div>
+            </div>
+            <div className={styles.imageContainer}>
+                <Image className={styles.image} src={BefAftImage}/>
+            </div>
+
+            <div className={styles.serviceData}>
+                <h1 className={styles.serviceTitle}>{props.value?.service || "[service]"} in {props.value?.city || "[city]"}</h1>
+            </div>
         </div>
     )
 }
